bce28a78eb39588c3aaeb87e839bf294c546bf20
jinho-kwak <kwakjinho77@gmail.com>
2021-03-03 14:12:56 +0900

    study_06.py

---
+++ study_06.py
@@ -0,0 +1,13 @@
+def solution(a, b):
+    day = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
+    mon = [31,29,31,30,31,30,31,31,30,31,30,31]
+    answer = ''
+    tmp = 0
+    if a == 1:
+        return day[b % 7]    
+    for i in range(a-1):
+        tmp += mon[i]
+    answer = day[(tmp + b) % 7]
+    return answer
+
+print(solution(5,24))
